7dbf4977df3a550aac0e79e6a6db0870113895e5
Vache Apinyan <apinyan.v@gmail.com>
2023-04-06 16:50:51 +0400

    feat(babyConstructor): added file and writed constructor function to baby.

---
+++ babyConstructor.js
@@ -0,0 +1,24 @@
+/* Baby Constructor
+Write a Baby constructor subclassing Person.
+Besides name and age, Baby takes a third argument to initialize favoriteToy.
+Besides the methods on Person.prototype, babies have the ability to .play():
+Should return a string "Playing with x", x being the favorite toy. */
+
+function Baby(name, age, favoriteToy) {
+  this.name = name;
+  this.age = age;
+  this.favoriteToy = favoriteToy;
+}
+
+Baby.prototype.play = function () {
+  const result = `${this.name} is an adult!`;
+
+  if (this.age < 18) {
+    return `${this.name} playing with ${this.favoriteToy}!`;
+  }
+  return result;
+};
+
+const babyPerson = new Baby("Bob", 10, "ball");
+
+console.log(babyPerson.play());
